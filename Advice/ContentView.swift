79747f44285ddb55c546c6c4917ea9ff27a5a1a8
Calum Couch <ccouch@lcs.on.ca>
2023-04-19 20:53:14 -0400

    setup nav title and where the wisdom is going to go.

--- Advice/ContentView.swift
+++ Advice/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("SOME PEICE OF WISDOM")
+                .font(.title)
+                .multilineTextAlignment(.center)
+            
+            
+            
         }
-        .padding()
+        .navigationTitle("Random Advice")
+        
     }
 }
 
