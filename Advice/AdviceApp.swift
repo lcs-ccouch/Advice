9787b93eca82773b52da1832b07e52860b387d6a
Calum Couch <ccouch@lcs.on.ca>
2023-04-19 20:34:08 -0400

    Initial Commit

---
+++ Advice/AdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdviceApp.swift
+//  Advice
+//
+//  Created by calum couch on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct AdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
